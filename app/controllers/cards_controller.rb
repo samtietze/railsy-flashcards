49b7ae182d9f3d1523fda8a9ea343b2c991706de
Eric Chiang & Sam Tietze <pair+byzantinist.samtietze@devbootcamp.com>
2017-10-09 16:32:39 -0500

    Write controller logic for creating game_guesses

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,8 +1,10 @@
 class CardsController < ApplicationController
 
   def show
+    p session[:user_id]
+    p session[:game_id]
     @deck = Deck.find(params[:deck_id])
     @card = Card.find(params[:id])
-    @guess = Guess.find_by(game_id: 1, card_id: @card.id)
+    @guess = Guess.find_by(game_id: session[:game_id], card_id: @card.id)
   end
 end
