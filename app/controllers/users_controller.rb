7eacc676257bb5cda2590204b351edc4139ddccd
Eric Chiang & Sam Tietze <pair+byzantinist.samtietze@devbootcamp.com>
2017-10-09 15:22:53 -0500

    Write new user registration form and controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,27 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to '/decks'
+    else
+      @errors = @user.errors.full_messages
+      render 'users/new'
+    end
   end
 
   def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 end
