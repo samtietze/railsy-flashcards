6a913ae37907d9c086cff5a2f8f68947e6942f20
Eric Chiang & Sam Tietze <pair+byzantinist.samtietze@devbootcamp.com>
2017-10-09 15:50:46 -0500

    Write sessions, users controllers and views for both. Append layout with links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,13 @@ class ApplicationController < ActionController::Base
   # end
 
   # helper_method :deck_game_path
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  helper_method :current_user
+
+  def authorize
+    redirect_to '/login' unless current_user
+  end
 end
