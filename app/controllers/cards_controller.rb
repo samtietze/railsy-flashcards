3b3819929e50192ad04c250a1e4633f89a291485
Eric Chiang & Sam Tietze <pair+byzantinist.samtietze@devbootcamp.com>
2017-10-09 17:52:13 -0500

    Write out some card guessing logic

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,8 +1,6 @@
 class CardsController < ApplicationController
 
   def show
-    p session[:user_id]
-    p session[:game_id]
     @deck = Deck.find(params[:deck_id])
     @card = Card.find(params[:id])
     @guess = Guess.find_by(game_id: session[:game_id], card_id: @card.id)
--- app/models/guess.rb
+++ app/models/guess.rb
@@ -2,5 +2,8 @@ class Guess < ApplicationRecord
   belongs_to :card
   belongs_to :game
   # belongs_to :guesser
+  def correct?(user_answer)
+    user_answer == self.card.answer
+  end
 
 end
